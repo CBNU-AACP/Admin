a9402cd1772c230c3e534f5d9cb8293ab8437ff2
동동이 <drdragon777@naver.com>
2021-11-28 21:42:15 +0900

    [FEAT] #5 - AllDataBases page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
+import DataBases from "./pages/AllDataBases";
 
 function App() {
+  const isAuthorized = localStorage.getItem("isAuthorized");
+
   return (
     <BrowserRouter>
+      {/* {!isAuthorized ? (
+        <Navigate replace to="/login" />
+      ) : (
+        <Navigate replace to="/" />
+      )} */}
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/" element={<DataBases />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/__mocks/DataBasesMocks.js
@@ -0,0 +1,44 @@
+const dataBases = [
+  {
+    id: 1,
+    title: "admin",
+  },
+  {
+    id: 2,
+    title: "checkmate",
+  },
+  {
+    id: 3,
+    title: "bluetooth",
+  },
+  {
+    id: 4,
+    title: "bluetooth",
+  },
+  {
+    id: 5,
+    title: "admin",
+  },
+  {
+    id: 6,
+    title: "checkmate",
+  },
+  {
+    id: 7,
+    title: "bluetooth",
+  },
+  {
+    id: 8,
+    title: "bluetooth",
+  },
+  {
+    id: 9,
+    title: "bluetooth",
+  },
+  {
+    id: 10,
+    title: "bluetooth",
+  },
+];
+
+export default dataBases;
