5fb26a0d411ca9a60f1a5753e172502dd0817b55
동동이 <drdragon777@naver.com>
2021-12-05 05:07:22 +0900

    [FEAT] #16 - Add AddTable in InsideDataBase page

--- client/src/pages/InsideDataBase/index.jsx
+++ client/src/pages/InsideDataBase/index.jsx
@@ -1,22 +1,33 @@
 import { useState, useEffect } from "react";
 import { Navigate, useParams } from "react-router-dom";
 import "./style.scss";
+import { useDispatch } from "react-redux";
 import Navigation from "../../components/Navigation";
+import AddTable from "../../components/AddTable";
+import TableMocks from "../../__mocks/TableMocks";
+import { getTables } from "../../store/tableSlice";
 
 export default function InsideDataBase(props) {
   const { dbName } = useParams();
+  const dispatch = useDispatch();
 
-  //   useEffect(() => {}, [dbName]); api 연결
+  useEffect(() => {
+    dispatch(getTables(TableMocks));
+  }, [dbName]);
 
   return (
     <div className="mainPage">
-      <div className="titleBox">
+      <header className="titleBox">
         <div className="title">
           <span className="dbName">{dbName}</span>
           <span>내부 조회</span>
         </div>
-      </div>
-      <Navigation />
+      </header>
+      <section className="buttonBar"></section>
+      <main className="mainContent">
+        <Navigation />
+        <AddTable />
+      </main>
     </div>
   );
 }
