993822851ca7c77c18e9396f4437f6d61848f016
동동이 <drdragon777@naver.com>
2021-12-05 05:05:59 +0900

    [FEAT] #16 - Add Navigation component's addTable button

--- client/src/components/Navigation/index.jsx
+++ client/src/components/Navigation/index.jsx
@@ -1,15 +1,32 @@
 import React, { useContext, useState, useEffect, useRef } from "react";
 import cx from "classnames";
 import "./style.scss";
-import TableMocks from "../../__mocks/TableMocks";
+import { AiOutlineDatabase } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import { add } from "../../store/tableSlice";
 
 export default function Natigation() {
+  const dispatch = useDispatch();
+  const tables = useSelector(state => state.table.tables);
+
+  console.log(tables);
   return (
     <nav className="navigation">
+      <button
+        type="button"
+        className="addTable"
+        onClick={() => {
+          dispatch(add(true));
+        }}>
+        <AiOutlineDatabase />
+        <p className="addText">테이블 추가</p>
+      </button>
       <p className="naviTitle">테이블 목록</p>
       <ul className="sideMenu">
-        {TableMocks.map(table => (
-          <li className="tableTitle">{table.title}</li>
+        {tables.map(table => (
+          <li key={table.id + table.title} className="tableTitle">
+            {table.title}
+          </li>
         ))}
       </ul>
     </nav>
