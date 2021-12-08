91e0fd2b216159f2335cad2d88b219435d687749
동동이 <drdragon777@naver.com>
2021-12-09 00:53:17 +0900

    [FEAT] #31 - Add Schema component

---
+++ client/src/__mocks/SchemaMocks.js
@@ -0,0 +1,52 @@
+const schemaMocks = [
+  {
+    Field: "name",
+    Type: "varchar(20)",
+    Null: "NO",
+    key: "",
+    Default: null,
+    Extra: "",
+  },
+  {
+    Field: "courseId",
+    Type: "varchar(20)",
+    Null: "NO",
+    key: "PRI",
+    Default: null,
+    Extra: "",
+  },
+  {
+    Field: "description",
+    Type: "text",
+    Null: "YES",
+    key: "",
+    Default: null,
+    Extra: "",
+  },
+  {
+    Field: "createAt",
+    Type: "datetime",
+    Null: "NO",
+    key: "",
+    Default: null,
+    Extra: "",
+  },
+  {
+    Field: "updateAt",
+    Type: "datetime",
+    Null: "NO",
+    key: "",
+    Default: null,
+    Extra: "",
+  },
+  {
+    Field: "userId",
+    Type: "varchar(20)",
+    Null: "NO",
+    key: "FOR",
+    Default: null,
+    Extra: "",
+  },
+];
+
+export default schemaMocks;
