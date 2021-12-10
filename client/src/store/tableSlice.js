22c26b4bb420d12407e8d5bf89069428389e6c7e
동동이 <drdragon777@naver.com>
2021-12-10 19:37:32 +0900

    [FEAT] #33 - Add getCurrentSchemaData, setCurrentSchema reducers

--- client/src/store/tableSlice.js
+++ client/src/store/tableSlice.js
@@ -7,6 +7,8 @@ const name = "table";
 const initialState = {
   tables: [],
   currentTable: "createTable",
+  currentSchema: [],
+  currentSchemaData: [],
 };
 
 export const tableSlice = createSlice({
@@ -14,14 +16,25 @@ export const tableSlice = createSlice({
   initialState,
   reducers: {
     getTables: (state, action) => {
+      // thunk 로 대체
       state.tables = [...action.payload];
       state.currentTable = "createTable";
+      state.currentSchema = [];
+      state.currentSchemaData = [];
     },
     setTable: (state, action) => {
       state.currentTable = action.payload;
+    },
+    getCurrentSchemaData: (state, action) => {
+      // thunk 로 대체
+      state.currentSchemaData = action.payload;
+    },
+    setCurrentSchema: (state, action) => {
+      state.currentSchema = action.payload;
     },
   },
   extraReducers: {},
 });
 
-export const { getTables, setTable } = tableSlice.actions;
+export const { getTables, setTable, getCurrentSchemaData, setCurrentSchema } =
+  tableSlice.actions;
