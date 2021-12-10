0f47bb4f365cd4f8b285937bfd6fe31268632818
L-o-g-a-n <xy.logandev@gmail.com>
2021-12-10 17:35:25 +0900

    [ADD] (#21) - add some schemas

---
+++ server/models/schemas/refreshToken.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize');
+
+module.exports = class RefreshToken extends Sequelize.Model { 
+  static init(sequelize) {
+    return super.init (
+      {
+        refreshToken: {
+          type: Sequelize.STRING(300),
+          allowNull: false,
+        }
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: "RefreshToken",
+        tableName: "refreshtokens",
+        paranoid: false,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+
+  static associate(db) {
+    db.RefreshToken.belongsTo(db.User, {foreignKey: "userId", targetKey: "id"});
+  }
+};
---
+++ server/models/schemas/user.js
@@ -0,0 +1,32 @@
+const Sequelize = require("sequelize");
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        userId: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+        },
+        userPassword: {
+          type: Sequelize.STRING(100),
+          allowNull: false,
+        }
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: "User",
+        tableName: "users",
+        paranoid: false,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+
+  static associate(db) {
+    db.User.hasOne(db.RefreshToken, {foreignKey: "userId", sourceKey: "id"});
+  }
+};
