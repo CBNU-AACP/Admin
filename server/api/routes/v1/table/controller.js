939b43f480d5f762086a6e544968aee37e5dfa7a
DongWooE <ehddn977@naver.com>
2021-12-11 14:06:53 +0900

    [ADD] (#30) - add codes about showing table & deleting table

--- server/api/routes/v1/table/controller.js
+++ server/api/routes/v1/table/controller.js
@@ -1,7 +1,21 @@
 const {poolQuery} =  require('../../../../utils/pool');
 const {makeEachColumnQueries} = require('./middleware');
-const {} = require('../../../../errors');
+const {TABLE_NOT_EXISTED} = require('../../../../errors');
 const { createResponse } = require('../../../../utils/response');
+const {MYSQL_DATABASE} = require('../../../../env');
+
+const showTables = async(req,res,next)=>{
+    try {
+        let q = `SHOW TABLES;`
+        let doc = await poolQuery(q);
+        doc = doc[0].map(item=>Object.values(item)[0]);
+        console.log(doc);
+        return res.json(createResponse(res,doc));
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+}
 
 const createTable = async(req,res,next)=>{
     const body = req.body;
@@ -21,4 +35,19 @@ const createTable = async(req,res,next)=>{
     }
 }
 
-module.exports = {createTable}
+const dropTable = async(req,res,next)=>{
+    const {name}= req.params;
+    try {
+        const table = await poolQuery(`SELECT COUNT(*) AS COUNT FROM Information_schema.tables
+        WHERE table_schema = '${MYSQL_DATABASE}' AND table_name = '${name}'
+        `)
+        if(table[0][0].COUNT == 0) return next(TABLE_NOT_EXISTED);
+        let q = `DROP TABLE ${name}`;        
+        const doc = await poolQuery(q);
+        return res.json(createResponse(res,doc));
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+}
+module.exports = {showTables, createTable, dropTable}
--- server/api/routes/v1/table/index.js
+++ server/api/routes/v1/table/index.js
@@ -2,6 +2,8 @@ const { Router } = require('express');
 const router = Router();
 const controller = require('./controller.js');
 
+router.get('/',controller.showTables);
+router.delete('/:name',controller.dropTable);
 router.post('/',controller.createTable);
 
 module.exports = router;
