b59148f263c2223a0b973cde4b33efd465a1485a
L-o-g-a-n <xy.logandev@gmail.com>
2021-12-14 00:33:42 +0900

    [FIX] (hotfix) - fix a params

--- server/api/routes/v1/row/index.js
+++ server/api/routes/v1/row/index.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 const controller = require('./controller.js');
 
-router.get('/:name',controller.showRows);
+router.get('/:tableName',controller.showRows);
 router.post('/',controller.createRows);
 router.post('/update', controller.updateRows);
 router.post('/delete', controller.deleteRow);
