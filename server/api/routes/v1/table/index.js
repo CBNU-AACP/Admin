0f2a1d7334d22d81084ff0007377f6dc5a391441
DongWooE <ehddn977@naver.com>
2021-12-13 20:49:54 +0900

    [ADD] (#37) - adding column type

--- server/api/routes/v1/table/index.js
+++ server/api/routes/v1/table/index.js
@@ -3,6 +3,7 @@ const router = Router();
 const controller = require('./controller.js');
 
 router.get('/',controller.showTables);
+router.get('/single/:name', controller.descTable);
 router.delete('/:name',controller.dropTable);
 router.post('/',controller.createTable);
 
